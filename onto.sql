7fb140f6bed7264d1113c0c9d90984764a86d058
Helge Dzierzon <helge.dzierzon@plantandfood.co.nz>
2015-10-20 16:17:48 +1300

    Removed redundant files

--- onto.sql
+++ onto.sql
@@ -1,53 +1,57 @@
 --
---,PostgreSQL,database,dump
+-- PostgreSQL database dump
 --
 
-SET statement_timeout=0;
-SET lock_timeout=0;
-SET client_encoding='UTF8';
-SET standard_conforming_strings=on;
-SET check_function_bodies=false;
-SET client_min_messages=warning;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
 
-SET search_path=public,pg_catalog;
+SET search_path = public, pg_catalog;
 
 --
---,Data,for,Name:,api_ontology;,Type:,TABLE,DATA;,Schema:,public;,Owner:,pinf
+-- Data for Name: api_ontology; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY api_ontology(id,name,tablename,owner,classname,is_entity,"group") FROM './onto.csv' WITH DELIMITER ',';
-20,Study,api_study,api,Study,t,api
-21,Tree,seafood_tree,seafood,Tree,t,seafood
-22,Crew,seafood_crew,seafood,Crew,t,seafood
-23,Species,api_species,api,Species,t,api
-24,Vessel,seafood_vessel,seafood,Vessel,t,seafood
-25,Trip,seafood_trip,seafood,Trip,t,seafood
-26,Treatment,api_treatment,api,Treatment,t,api
-27,SampleMethod,api_sample_method,api,SampleMethod,t,api
-28,Instrument,api_instrument,api,Instrument,t,api
-29,Tow,seafood_tow,seafood,Tow,t,seafood
-30,City,seafood_city,seafood,City,t,seafood
-31,BioSubject,api_bio_subject,api,BioSubject,t,api
-33,Primer,genotype_primer,genotype,Primer,t,genotype
-34,PrimerType,genotype_primer_type,genotype,PrimerType,t,genotype
-35,PrimerOb,genotype_primer_ob,genotype,PrimerOb,t,genotype
-36,Marker,genotype_marker,genotype,Marker,t,genotype
-69,Term,api_term,api,Term,t,api
-70,DataSource,api_datasource,api,DataSource,t,api
-72,Staff,seafood_staff,seafood,Staff,t,seafood
-74,Gene,gene_expression_gene,gene_expression,Gene,t,gene_expression
-76,Target,gene_experssion_target,gene_experssion,Target,t,gene_experssion
-32,Fish,seafood_fish,seafood,Fish,t,seafood
-83,StudyArea,api_studyarea,api,StudyArea,t,api
-84,Protein,api_protein,api,Protein,t,api
-85,StudyGroup,api_studygroup,api,StudyGroup,t,api
-86,Tissue,api_tissue,api,Tissue,t,api
-
-
-SELECT pg_catalog.setval('api_ontology_id_seq',86,true);
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (20, 'Study', 'api_study', 'api', NULL, 'Study', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (21, 'Tree', 'seafood_tree', 'seafood', NULL, 'Tree', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (22, 'Crew', 'seafood_crew', 'seafood', NULL, 'Crew', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (23, 'Species', 'api_species', 'api', NULL, 'Species', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (24, 'Vessel', 'seafood_vessel', 'seafood', NULL, 'Vessel', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (25, 'Trip', 'seafood_trip', 'seafood', NULL, 'Trip', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (26, 'Treatment', 'api_treatment', 'api', NULL, 'Treatment', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (27, 'SampleMethod', 'api_sample_method', 'api', NULL, 'SampleMethod', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (28, 'Instrument', 'api_instrument', 'api', NULL, 'Instrument', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (29, 'Tow', 'seafood_tow', 'seafood', NULL, 'Tow', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (30, 'City', 'seafood_city', 'seafood', NULL, 'City', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (31, 'BioSubject', 'api_bio_subject', 'api', NULL, 'BioSubject', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (33, 'Primer', 'genotype_primer', 'genotype', NULL, 'Primer', true, 'genotype');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (34, 'PrimerType', 'genotype_primer_type', 'genotype', NULL, 'PrimerType', true, 'genotype');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (35, 'PrimerOb', 'genotype_primer_ob', 'genotype', NULL, 'PrimerOb', true, 'genotype');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (36, 'Marker', 'genotype_marker', 'genotype', NULL, 'Marker', true, 'genotype');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (69, 'Term', 'api_term', 'api', NULL, 'Term', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (70, 'DataSource', 'api_datasource', 'api', NULL, 'DataSource', true, 'api');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (72, 'Staff', 'seafood_staff', 'seafood', NULL, 'Staff', true, 'seafood');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (74, 'Gene', 'gene_expression_gene', 'gene_expression', NULL, 'Gene', true, 'gene_expression                                                              ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (76, 'Target', 'gene_experssion_target', 'gene_experssion', NULL, 'Target', true, 'gene_experssion                                                        ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (32, 'Fish', 'seafood_fish', 'seafood', NULL, 'Fish', true, 'seafood                                                                                            ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (83, 'StudyArea', 'api_studyarea', 'api', NULL, 'StudyArea', true, 'api                                                                                         ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (84, 'Protein', 'api_protein', 'api', NULL, 'Protein', true, 'api                                                                                               ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (85, 'StudyGroup', 'api_studygroup', 'api', NULL, 'StudyGroup', true, 'api                                                                                      ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (86, 'Tissue', 'api_tissue', 'api', NULL, 'Tissue', true, 'api                                                                                                  ');
+INSERT INTO api_ontology (id, name, tablename, owner, description, classname, is_entity, "group") VALUES (1, 'Genotype', 'genotype_genotype', 'genotype', '', 'Genotype', true, 'genotype');
 
 
 --
---,PostgreSQL,database,dump,complete
+-- Name: api_ontology_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('api_ontology_id_seq', 1, true);
+
+
+--
+-- PostgreSQL database dump complete
 --
 
