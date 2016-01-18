25b2aab97bf1350c25b262b8f167f8dc37c25d5e
Badi James <Badi.James@plantandfood.co.nz>
2016-01-18 14:01:37 +1300

    Maybe this will do the trick????

--- kaka/settings.py
+++ kaka/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from mongoengine import connect, register_connection
+from mongoengine import connect, register_connection, ConnectionError
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
