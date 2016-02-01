2ab03c739122c42ed77099c07bdacfbe15a8b8df
Badi <badi.james@plantandfood.co.nz>
2016-02-02 12:44:47 +1300

    I might have fixed it...

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+MONGODB=`ping -c 1 mongo | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+echo "Waiting for startup.."
+until curl http://${MONGODB}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+  printf '.'
+  sleep 1
+done
+
+echo curl http://${MONGODB}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
+echo "Started.."
+
+echo SETUP.sh time now: `date +"%T"`
+
 mongo --host ${MONGODB}:27017 <<EOF
    var cfg = {
         "_id": "rs",
